521cfdedd445606db8e5d9c8eb360b6a38528c55
Mark Sikaundi <sikaundimark@gmail.com>
2021-06-27 15:43:38 +0200

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,84 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "css3",
+				weight: 12.3
+			}, {
+				text: "Qazi",
+				weight: 8
+			}, {
+				text: "javascript",
+				weight: 14
+			}, {
+				text: "coursera",
+				weight: 3
+			}, {
+				text: "programming",
+				weight: 7
+			}, {
+				text: "python",
+				weight: 10
+			}, {
+				text: "java",
+				weight: 9
+			}, {
+				text: "html",
+				weight: 15
+			}, {
+				text: "Harrison",
+				weight: 7
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Full-Stack Web Developer.", "love everything about computer.", "also teach programming to my fellow students at Phoenix University.", "how to solving problems."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
